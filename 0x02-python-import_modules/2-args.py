0698c75d858d536790ff467598dc50f1850bb7d2
Sincerekiko <sincerekiko@gmail.com>
2023-12-04 04:49:01 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,15 @@
-import sys
+#!/usr/bin/python3
 
-num_args = len(sys.argv) - 1  # Subtract 1 for the script name itself
-args_list = sys.argv[1:]  # Exclude the script name from arguments
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
 
-if num_args == 0:
-    print("0 arguments.")
-else:
-    print(f"{num_args} {'argument:' if num_args == 1 else 'arguments:'}")
-    for i, arg in enumerate(args_list, 1):
-        print(f"{i}: {arg}")
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
