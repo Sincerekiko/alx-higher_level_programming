971b40cadc413f93315bd9917206178199bf17d8
Sincerekiko <sincerekiko@gmail.com>
2024-03-15 12:39:42 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -2,8 +2,15 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
 USE hbtn_0e_0_usa;
 CREATE TABLE IF NOT EXISTS states ( 
-	    id INT NOT NULL AUTO_INCREMENT, 
-	    name VARCHAR(256) NOT NULL,
-	    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+
+guillaume@ubuntu:~/0x0F$ cat 0-select_states.sql | mysql -uroot -p
+Enter password: 
+guillaume@ubuntu:~/0x0F$ ./1-filter_states.py root root hbtn_0e_0_usa
+(4, 'New York')
+(5, 'Nevada')
+guillaume@ubuntu:~/0x0F$
