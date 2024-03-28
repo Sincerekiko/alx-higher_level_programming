3bc466fbd92520bd6c9ae3318eab215ba6f7c25f
Sincerekiko <sincerekiko@gmail.com>
2024-03-28 14:15:34 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-#Send a request to that URL, and displays the size of the body of the response
-curl -s "$1" | wc -c
+
+# Check if the user has provided a URL
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Get the URL from the command line argument
+URL=$1
+
+# Send a request to the URL using curl and store the response body in a variable
+response=$(curl -sI "$URL" | grep -i Content-Length | awk '{print $2}')
+
+# Display the size of the body of the response in bytes
+echo "$response"
